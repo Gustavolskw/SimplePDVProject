807cc1759d2e2ef65d97e3559356d14acbb7930a
Gustavo Luis <gustavolschmidt13@gmail.com>
2024-12-15 15:41:00 -0300

    product creation method add on service

--- service/src/main/java/com/web/service/application/service/ProductService.java
+++ service/src/main/java/com/web/service/application/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.web.service.application.service;
 
 import com.web.service.application.dto.ProductCreationDTO;
+import com.web.service.domain.exception.AlreadyExistsException;
 import com.web.service.domain.exception.ListEmptyException;
 import com.web.service.domain.model.Product;
 import com.web.service.domain.repository.ProductRepository;
@@ -8,10 +9,12 @@ import com.web.service.presentation.viewModel.ProductResponseDTO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.internal.bytebuddy.implementation.bytecode.Throw;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -46,5 +49,16 @@ public class ProductService {
         List<Product> products = productRepository.findAll();
         if(products.isEmpty())throw new ListEmptyException("Product list is empty");
         return products.stream().map(ProductResponseDTO::new).toList();
+    }
+
+
+    private Product buildProductFromDTO(ProductCreationDTO productCreationDTO) {
+        Product product = new Product();
+        Optional<Product> prodt = productRepository.findByName(productCreationDTO.name());
+        if(prodt.isPresent())throw new AlreadyExistsException("Product already exists");
+        product.setName(productCreationDTO.name());
+        product.setDescription(productCreationDTO.description());
+        product.setValue(productCreationDTO.value());
+        return product;
     }
 }
--- service/src/main/java/com/web/service/domain/repository/ProductRepository.java
+++ service/src/main/java/com/web/service/domain/repository/ProductRepository.java
@@ -3,5 +3,8 @@ package com.web.service.domain.repository;
 import com.web.service.domain.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String productName);
 }
