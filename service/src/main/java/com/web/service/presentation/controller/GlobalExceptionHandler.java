a5d00cc21ef3ab53e4d0118b1e7e1a71be5e5898
Gustavolskw <gustavolschmidt13@gmail.com>
2025-02-07 19:31:09 -0300

    reafctor: changing the output for Exception handlers, to show the class that thrown the Error

--- service/src/main/java/com/web/service/presentation/controller/GlobalExceptionHandler.java
+++ service/src/main/java/com/web/service/presentation/controller/GlobalExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.io.IOException;
-import java.net.ConnectException;
 
 @RestControllerAdvice
 @Slf4j
@@ -21,62 +20,62 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ListEmptyException.class)
     public ResponseEntity<ApiResponse> EmptyListResponse(ListEmptyException e) {
-        return ResponseEntity.ok().body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.ok().body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ApiResponse> EntityNotFoundResponse(EntityNotFoundException e) {
-        return ResponseEntity.status(404).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(404).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(CannotCreateTransactionException.class)
     public ResponseEntity<ApiResponse> ConnectException(CannotCreateTransactionException e) {
-        return ResponseEntity.status(400).body(new ApiResponse("Conexão Perdida com o Servidor de Banco de dados!",e.getMessage()) );
+        return ResponseEntity.status(400).body(new ApiResponse("Conexão Perdida com o Servidor de Banco de dados!",e.getClass().getName()) );
     }
 
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ApiResponse> UsernameNotFoundException(UsernameNotFoundException e) {
-        return ResponseEntity.status(404).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(404).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(IOException.class)
     public ResponseEntity<ApiResponse> IOException(IOException e) {
-        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(ServletException.class)
     public ResponseEntity<ApiResponse> ServletException(ServletException e) {
-        return ResponseEntity.status(400).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(400).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(JwtException.class)
     public ResponseEntity<ApiResponse> JwtException(JwtException e) {
-        return ResponseEntity.status(401).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(401).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> Exception(Exception e) {
         log.error(e.getMessage(), e);
-        return ResponseEntity.status(400).body(new ApiResponse("Erro Inserperado Ocorreu!",e.getMessage()) );
+        return ResponseEntity.status(400).body(new ApiResponse("Erro Inserperado Ocorreu!",e.getClass().getName()) );
     }
 
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ApiResponse> ValidationException(ValidationException e) {
-        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseEntity<ApiResponse> AlreadyExistsException(AlreadyExistsException e) {
-        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
     @ExceptionHandler(UnprocessableAction.class)
     public ResponseEntity<ApiResponse> UnprocessableAction(UnprocessableAction e) {
-        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
     @ExceptionHandler(OrderProcessError.class)
     public ResponseEntity<ApiResponse> OrderProcessError(OrderProcessError e) {
-        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),null) );
+        return ResponseEntity.status(422).body(new ApiResponse(e.getMessage(),e.getClass().getName()) );
     }
 
 
