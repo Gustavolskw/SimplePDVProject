d5600031f427e8ea7d3f65547c15164bb731de47
Gustavolskw <gustavolschmidt13@gmail.com>
2025-02-11 14:19:14 -0300

    fix:update on ProductResponseDTO to send the typeId on the body not only the Type Description of the product

--- service/src/main/java/com/web/service/presentation/viewModel/ProductResponseDTO.java
+++ service/src/main/java/com/web/service/presentation/viewModel/ProductResponseDTO.java
@@ -4,8 +4,8 @@ import com.web.service.domain.model.Product;
 
 import java.math.BigDecimal;
 
-public record ProductResponseDTO(Long id, String name, String description, String type, Boolean status,  BigDecimal value,  String imageUrl) {
+public record ProductResponseDTO(Long id, String name, String description, String type, Long typeId, Boolean status,  BigDecimal value,  String imageUrl) {
     public ProductResponseDTO(Product p){
-        this(p.getId(), p.getName(), p.getDescription(),p.getProductType().getName(), p.getStatus(), p.getValue(), p.getImageUrl());
+        this(p.getId(), p.getName(), p.getDescription(),p.getProductType().getName(), p.getProductType().getId(), p.getStatus(), p.getValue(), p.getImageUrl());
     }
 }
