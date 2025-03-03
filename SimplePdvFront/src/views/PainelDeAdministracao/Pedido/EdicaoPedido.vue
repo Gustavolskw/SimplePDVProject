c144988e589b5c095e27adff5ade808d6495764c
Gustavolskw <gustavolschmidt13@gmail.com>
2025-03-03 17:08:24 -0300

    Feat:Adding new endpoint to update and process the data of an order.

--- SimplePdvFront/src/views/PainelDeAdministracao/Pedido/EdicaoPedido.vue
+++ SimplePdvFront/src/views/PainelDeAdministracao/Pedido/EdicaoPedido.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="row w-100">
-    <div class="col-1">
+  <div class="row w-100 ps-md-5 ps-lg-5 ps-2">
+    <div class="col-2 col-md-1 mb-5">
       <button
         type="button"
         class="btn-close ms-2 fs-4"
-        @click.prevent="handleReturnToPedidos"
+        @click="handleReturnToPedidos"
       ></button>
     </div>
-    <section class="col-11">
+    <div class="col-10 col-md-11">
+      <button class="btn btn-primary ms-2 fs-4" :disabled="!isOrderChanged">
+        Atualizar Pedido
+      </button>
+    </div>
+    <section class="col-12 justify-content-center ps-4 ps-md-5">
       <div class="d-flex">
         <p class="fw-bold fs-2">#{{ orderData.id }}</p>
       </div>
       <div>
-        <EditPedidoForm :orderData="orderData"></EditPedidoForm>
+        <EditPedidoForm
+          :orderData="orderData"
+          @ORDER_CHANGED="isOrderChanged = true"
+          @ORDER_RESET="isOrderChanged = false"
+        ></EditPedidoForm>
       </div>
     </section>
   </div>
@@ -29,19 +38,19 @@ const router = useRouter();
 const route = useRoute();
 const orderId = ref(route.params.id);
 const orderData = ref([]);
+const isOrderChanged = ref(false);
 
 onMounted(() => {
   fetchOrderData(orderId.value);
 });
 
 const handleReturnToPedidos = () => {
-  router.push({ name: "administracao-pedidos" });
+  router.push({ path: "/administracao/pedidos" });
 };
 const fetchOrderData = async (id) => {
   try {
     const response = await orderService.getOrderById(id);
-
-    console.log(response);
+    console.log("Pedido-request: " + response);
     if (response.status == 200) {
       orderData.value = response.data.data;
       if (
@@ -50,6 +59,7 @@ const fetchOrderData = async (id) => {
       ) {
         handleReturnToPedidos();
       }
+      console.log("Pedido-body: " + orderData.value);
     }
   } catch (error) {
     console.log(error);
